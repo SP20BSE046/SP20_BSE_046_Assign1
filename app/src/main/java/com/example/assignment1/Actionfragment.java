5ee603f157e9629ad63f04fdb703f54df2155404
Usman Taufique <65678234+SP20BSE046@users.noreply.github.com>
2022-04-21 18:16:23 +0500

    Updating

---
+++ app/src/main/java/com/example/assignment1/Actionfragment.java
@@ -0,0 +1,45 @@
+package com.example.assignment1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Actionfragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Actionfragment extends Fragment {
+    Button input;
+    Button calc;
+
+    public Actionfragment() {
+        // Required empty public constructor
+    }
+
+
+    public static Actionfragment newInstance() {
+        Actionfragment fragment = new Actionfragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_actionfragment, container, false);
+        input=view.findViewById(R.id.inputbtn);
+        calc=view.findViewById(R.id.Calcbtn);
+        return view;
+    }
+}
--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FragmentManager fragmentManager;
+    private Actionfragment actionfragment;
+    private MessageFragment messageFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        actionfragment=Actionfragment.newInstance();
+        messageFragment=MessageFragment.newInstance();
+        fragmentTransaction.replace(R.id.LeftFL,actionfragment);
+        fragmentTransaction.replace(R.id.RightFL, messageFragment);
+        fragmentTransaction.commit();
     }
 }
